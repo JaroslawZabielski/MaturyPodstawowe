9dd031d82f88163970a7d20a0d9edf98154f6aa5
jzabielski <zabielskijarek@gmail.com>
2019-10-08 14:34:04 +0200

    Zadanie6Matura2019PP
    Wstęp do zad. 6 matura 2019 PP

---
+++ matura2019PP/zadanie6Matura2019PP.py
@@ -0,0 +1,12 @@
+"""
+Zadanie 6 Matura 2019 PP
+(C) Jarosław Zabielski, październik 2019
+LO Sobolew
+"""
+
+with open("dane.txt", "r") as plik:
+    nr = 0
+    for pesel in plik:
+        pesel = pesel.strip()
+        nr += 1
+        print(nr, ". ", pesel)
