77456eda9f32b48f5590c98fa743e2a0433041e7
arcadiuss <56313499+arcadiuss@users.noreply.github.com>
2019-11-08 10:33:15 +0100

    zadanie5matura2018pp
    Zad5

---
+++ matura2018PP/zadanie5matura2018pp/zadanie5matura2018pp.py
@@ -0,0 +1,45 @@
+"""
+Zad. 5 matura 2018 PP
+(C) Arkadiusz Zgódka, październik 2019
+LO w Sobolewie
+"""
+
+def suma_cyfr(n):
+    suma = 0
+    while n > 0:
+        suma += n % 10
+        n = n // 10
+    return suma
+
+
+with open("liczby.txt", "r") as plik:
+    max_parzysta = 0
+    palindromy = ''
+    cyfrony30 = ''
+    suma_wszystkich_cyfr = 0
+    for n in plik:
+        n = n.strip()
+        # 5.2
+        if n == n[::-1]:
+            palindromy = palindromy + n +'\n'
+
+        # 5.1
+        n = int(n)
+        if n % 2 == 0:
+            if n > max_parzysta:
+                max_parzysta = n
+
+        # 5.3
+        suma_wszystkich_cyfr += suma_cyfr(n)
+        if suma_cyfr(n) > 30:
+            cyfrony30 += str(n) + '\n'
+
+with open("wyniki5.txt", "w") as wyniki:
+    wyniki.write("5.1\n"
+                 + str(max_parzysta)
+                 + "\n\n5.2\n"
+                 + palindromy
+                 + "\n5.3\n"
+                 + cyfrony30
+                 + 'Suma wszystkich cyfr: '
+                 + str(suma_wszystkich_cyfr))
